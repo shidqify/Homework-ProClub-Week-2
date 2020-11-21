2a71eaeeda9ce21be4f1c9ab4bccca57c4977a63
Shidqi Yusri <74455985+shidqify@users.noreply.github.com>
2020-11-21 21:43:02 +0700

    Changing some variable names

--- Homework 2.py
+++ Homework 2.py
@@ -1,22 +1,23 @@
 A = input('Klub A : ')
 B = input('Klub B : ')
 
-list_hasil = []
-pertandingan = 1
+list_result = []
+match = 1
+
 while True:
-    hasilA, hasilB = map(int, input(f'Pertandingan {pertandingan} : ').split('\t'))
-    if hasilA < 0 or hasilB < 0:
+    resultA, resultB = map(int, input(f'Pertandingan {match} : ').split('\t'))
+    if resultA < 0 or resultB < 0:
         break
-
-    if hasilA > hasilB:
-        list_hasil.append(A)
-    elif hasilA < hasilB:
-        list_hasil.append(B)
+        
+    if resultA > resultB:
+        list_result.append(A)
+    elif resultA < resultB:
+        list_result.append(B)
     else:
-        list_hasil.append('Draw')
-    pertandingan += 1
+        list_result.append('Draw')
+    match += 1
 
-for i in range(len(list_hasil)):
-    print(f'Hasil {i+1} : {list_hasil[i]}')
+for i in range(len(list_result)):
+    print(f'Hasil {i+1} : {list_result[i]}')
 
 print('Pertandingan selesai')   
