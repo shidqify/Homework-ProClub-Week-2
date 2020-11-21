299e3d91f505e3793aab0a3aad2a44dfa068596e
Shidqi Yusri <74455985+shidqify@users.noreply.github.com>
2020-11-21 21:53:58 +0700

    Changing some minor issues

--- Homework 1.py
+++ Homework 1.py
@@ -4,7 +4,6 @@ bMax = 0
 
 def hitungMinMax(arrBerat):
     global bMin, bMax
-
     arrBerat.sort()
     bMin += arrBerat[0]
     bMax += arrBerat[-1]
@@ -12,16 +11,13 @@ def hitungMinMax(arrBerat):
     print('''Berat balita minimum : {:.2f} kg
 Berat balita maksimum : {:.2f} kg'''.format(bMin, bMax))
 
-
 def rerata(arrBerat):
     total = 0
-
     for i in arrBerat:
-        total += i
-    
+        total += i  
+        
     rata = total / len(arrBerat)
     print('Rerata berat balita: {:.2f}'.format(rata))
-
 
 print('Masukkan Banyak Data Berat Balita :', end=" ")
 n = int(input())
